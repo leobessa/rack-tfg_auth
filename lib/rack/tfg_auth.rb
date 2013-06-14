aafbfebf063016de4a20891614f8cb1b84cfed78
leobessa <leobessa@gmail.com>
2013-06-14 20:36:31 -0300

    add request timing spec

--- lib/rack/tfg_auth.rb
+++ lib/rack/tfg_auth.rb
@@ -46,11 +46,11 @@ module Rack
       token = header.to_s.match(/^TFG (.*)/) { |m| m[1] }
       if token
         begin
-          values = Hash[token.split(',').map do |value|
-            value.strip!                      # remove any spaces between commas and values
-            key, value = value.split(/\=\"?/) # split key=value pairs
-            value.chomp!('"')                 # chomp trailing " in value
-            value.gsub!(/\\\"/, '"')          # unescape remaining quotes
+          values = Hash[token.split(%r/"(?:,|;|\s)\s*/).map do |value|
+            value.strip!                         # remove any spaces between commas and values
+            key, value = value.split(%r/=(.+)?/) # split key=value pairs
+            value.chomp!('"')                    # chomp trailing " in value
+            value.gsub! %r/^"|"$/, ''            # unescape remaining quotes
             [key.to_sym, value]
           end]
           [values.delete(:signature), values]
@@ -66,7 +66,7 @@ module Rack
     def default_block(signature, options, env)
       return false unless signature && options && options[:client] && options[:timestamp]
       return false unless valid_timestamp?(options[:timestamp])
-      secret = secret_for_client(auth_options[:client])
+      secret = secret_for_client(options[:client])
       return false unless secret
       signature == expected_signature(secret,env,options)
     end
@@ -78,13 +78,13 @@ module Rack
 
     def valid_timestamp?(timestamp)
       client_time = timestamp.to_i
-      now = Time.now.to_i
-      time_abs_diff   = (now - time_in_seconds).abs
-      (time_abs_diff < 4.hours)
+      now = @options.fetch(:now){ Time.now.to_i }
+      time_abs_diff   = (now - client_time).abs
+      (time_abs_diff < (4 * 3600))
     end
 
     def secret_for_client(client)
-      @options.fetch(:client_secrets){Hash.new}[client]
+      @options.fetch(:client_secrets) { Hash.new }[client]
     end
 
     def build_string_to_sign(env, options)
@@ -103,7 +103,7 @@ module Rack
 
     def hmac_signature(secret, string_to_sign)
       @digest ||= OpenSSL::Digest::Digest.new('sha256')
-      Base64.encode64(OpenSSL::HMAC.digest(digest, secret, string_to_sign)).chomp
+      Base64.encode64(OpenSSL::HMAC.digest(@digest, secret, string_to_sign)).chomp
     end
 end
 end
--- spec/rack_tfg_auth_spec.rb
+++ spec/rack_tfg_auth_spec.rb
@@ -1,6 +1,17 @@
 require 'rack'
 require 'rack/tfg_auth'
 
+class Fixnum
+  def hours
+    self * 3600
+  end
+  def minutes
+    self * 60
+  end
+  def second
+    self
+  end
+end
 describe Rack::TfgAuth do
 
   Endpoint = Rack::Response.new("OK")
@@ -69,6 +80,36 @@ describe Rack::TfgAuth do
       app.call(env)
     end
 
+  end
+
+  context "default block" do
+    it "denies access to requests without authorization header" do
+      app = build_app
+      result = app.call({})
+      result.status.should eq 401
+    end
+    it "accepts access to requests that are almost 4 hours old" do
+      now = Time.utc(2013,6,14,12).to_i
+      app = build_app(:now => now, :client_secrets => {"fake" => "secret"})
+      client_time = (now - 4.hours + 1.second).to_i
+      env = {
+        "HTTP_AUTHORIZATION" => %(TFG signature="HGPBSMJWistgrkOKYxfVhhrZ7X4SBPUjmuVyn36ywP0=", client="fake", timestamp="#{client_time}"),
+        "rack.input" => StringIO.new
+      }
+      Endpoint.should_receive(:call).with(env)
+      result = app.call(env)
+    end
+    it "rejects access to requests that are more than 4 hours old" do
+      now = Time.utc(2013,6,14,12).to_i
+      app = build_app(:now => now, :client_secrets => {"fake" => "secret"})
+      client_time = (now - 4.hours - 1.second).to_i
+      env = {
+        "HTTP_AUTHORIZATION" => %(TFG signature="hb0aXg2DL3I+VrfvGH7vm1DkgFF1l7e6rdbemoE2q1s=", client="fake", timestamp="#{client_time}"),
+        "rack.input" => StringIO.new
+      }
+      result = app.call(env)
+      result.status.should eq 401
+    end
   end
 
   context "when the block returns true" do
